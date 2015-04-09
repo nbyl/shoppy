9522b0a381a5812529851be8ad788caa717b0395
Nicolas Byl <nicolas.byl@netpioneer.de>
2015-04-09 22:40:25 +0200

    added new registry service as eureka server

--- registry-service/src/main/java/com/github/nbyl/shoppy/registry/Application.java
+++ registry-service/src/main/java/com/github/nbyl/shoppy/registry/Application.java
@@ -1,13 +1,13 @@
-package com.github.nbyl.shoppy.frontend;
+package com.github.nbyl.shoppy.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @EnableAutoConfiguration
-@EnableEurekaClient
+@EnableEurekaServer
 public class Application {
 
     public static void main(String[] args) {
